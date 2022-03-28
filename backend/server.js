f20040f825664f233a0f3bb5bcbfa37f60e7873f
yongseongjeon <yongseong.jeon95@gmail.com>
2022-03-28 11:45:13 +0900

    refactor: path.resolve()에서 node에서 기본 제공해주는 __dirname으로 변경

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
-import express from "express";
-import path from "path";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
-const __dirname = path.resolve();
 
 app.listen(PORT, () => {
   console.log(`✅ listening on http://localhost:${PORT}/`);
